6b27c570bcc69b6e5827687deefef0e8a9d945cd
Patrícia Betíková <patriciabetikova@gmail.com>
2018-12-03 15:54:34 +0100

    init

---
+++ precache-manifest.9d1df50d3a13bcf12fc769959b2c80bd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6a173dbd5d8b13242ab",
+    "url": "/static/js/main.f6a173db.chunk.js"
+  },
+  {
+    "revision": "a8f90a1f5dae2ded908a",
+    "url": "/static/js/1.a8f90a1f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0d7634e1c245991cba3afb860a7773f",
+    "url": "/index.html"
+  }
+];
