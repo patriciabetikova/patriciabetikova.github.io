6c8155a6452cb352ecad7f8d55a2326380a30abe
Patrícia Betíková <patriciabetikova@Patricias-MacBook-Pro.local>
2019-07-17 11:49:31 +0200

    update

---
+++ precache-manifest.1ec384bb5d50bef40302d2adde43bd0f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3efb439d636560d217a88a78939107",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8782733b9189e2948c0c",
+    "url": "/static/js/2.7a575f5a.chunk.js"
+  },
+  {
+    "revision": "86b23d4525c119454e26",
+    "url": "/static/js/main.36df2976.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
